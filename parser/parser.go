e76d19d101086b8c8ca308647d319cf88274b715
SeniorMacBus <SeniorMacBus@protonmail.com>
2025-02-24 00:54:29 +0100

    finished basics of a parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,11 @@
 package parser
+
+import "strings"
+
+func ParseInput(inp string) (string, []string) {
+	args := strings.Fields(inp)
+	command := args[0]
+	text := args[1:]
+
+	return command, text
+}
